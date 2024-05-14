67f43dc9f4d29ab80d3d8355fed1675c10581ce6
Chinh-Leesandy <maichinh17092002@gmail.com>
2024-05-15 06:55:42 +0700

    Chinh push fix tk

--- ManageStore_FE/src/components/tk/tkthang/TKHoaDonKH.jsx
+++ ManageStore_FE/src/components/tk/tkthang/TKHoaDonKH.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export const TKHoaDonKH = () => {
     const location = useLocation();
     const invoiceProducts = location.state.invoiceProducts;
-    const handleView= () =>{}
+    const formatCurrency = (value) => {
+        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+    };
   return (
     <div className='container'>
-        <h2 className='text-center'>Danh Sách Hóa Đơn Của Khách Hàng</h2>
+        <h2 className='text-center'>Hóa Đơn Chi Tiết</h2>
       <table className='table table-striped'>
         <thead>
           <tr className = 'text-center'>
-            <td>Tổng sản phẩm</td>
-            <td>Tổng tiền</td>
-            <td>Action</td>
+            <td>Ảnh</td>
+            <td>Tên Sản phẩm</td>
+            <td>Giá tiền</td>
+            <td>Số sản phẩm</td>
+            <td>Thành tiền</td>
           </tr>
         </thead>
         <tbody>
           {invoiceProducts.map((items)=> (
             <tr className='text-center'>
+              <td><img style={{width: '50px', height : '50px'}} src={items.imageUrl} alt="" srcset="" /></td> 
+              <td>{items.product.name}</td> 
+              <td>{formatCurrency(items.product.price)}</td> 
               <td>{items.amount}</td>
-              <td>{items.totalPrice}</td>
-              <td>
-                <button type="button" class="btn btn-outline-warning" onClick={() => handleView(items.invoiceProducts)}>Xem chi tiết</button>
-              </td>
+              <td>{formatCurrency(items.totalPrice)}</td>
             </tr>
           ))}
         </tbody>
