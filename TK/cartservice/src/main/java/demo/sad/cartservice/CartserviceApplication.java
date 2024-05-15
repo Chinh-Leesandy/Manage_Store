19708f8c6ac7b0e633c471d5e21f2a3f5ea7fce0
Chinh-Leesandy <maichinh17092002@gmail.com>
2024-05-15 07:10:12 +0700

    Chinh push be TK Hiep

---
+++ TK/cartservice/src/main/java/demo/sad/cartservice/CartserviceApplication.java
@@ -0,0 +1,18 @@
+package demo.sad.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
+@SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
+public class CartserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartserviceApplication.class, args);
+	}
+
+}
