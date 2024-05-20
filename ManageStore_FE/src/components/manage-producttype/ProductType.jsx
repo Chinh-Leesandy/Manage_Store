a2dab89aad50d4bb9dc20fb28e7ffdc253bccc1c
Chinh <101963688+Chinh-Leesandy@users.noreply.github.com>
2024-05-20 23:41:18 +0700

    thêm classname cho validation

--- ManageStore_FE/src/components/manage-producttype/ProductType.jsx
+++ ManageStore_FE/src/components/manage-producttype/ProductType.jsx
@@ -106,22 +106,22 @@ export const ProductType = () => {
         <div className="col-md-6">
           <label htmlFor="ten" className="form-label">Tên loại mặt hàng:<span style={{ color: 'red', marginLeft: '5px' }}>*</span></label>
           <input type="text" className={validation.ten ? 'form-control is-invalid' : 'form-control'} id="ten" name="ten" value={productType.ten} onChange={(e) => setProductType({...productType, ten: e.target.value})} required/>
-          {validation.ten && <div className="invalid-feedback">{validation.ten}</div>}
+          {validation.ten && <div className="invalid-feedback validation-ten">{validation.ten}</div>}
         </div>
         <div className="col-md-6">
           <label htmlFor="ncc" className="form-label">Nhà cung cấp:<span style={{ color: 'red', marginLeft: '5px' }}>*</span></label>
           <input type="text" className={validation.ncc ? 'form-control is-invalid' : 'form-control'} id="ncc" name="ncc" value={productType.ncc} onChange={(e) => setProductType({...productType, ncc: e.target.value})} required/>
-          {validation.ncc && <div className="invalid-feedback">{validation.ncc}</div>}
+          {validation.ncc && <div className="invalid-feedback validation-ncc">{validation.ncc}</div>}
         </div>
         <div className="col-md-6">
           <label htmlFor="thoigiannhap" className="form-label">Thời gian nhập:<span style={{ color: 'red', marginLeft: '5px' }}>*</span></label>
           <input type="date" className={validation.thoigiannhap ? 'form-control is-invalid' : 'form-control'} id="thoigiannhap" name="thoigiannhap" value={productType.thoigiannhap} onChange={(e) => setProductType({...productType, thoigiannhap: e.target.value})} required/>
-          {validation.thoigiannhap && <div className="invalid-feedback">{validation.thoigiannhap}</div>}
+          {validation.thoigiannhap && <div className="invalid-feedback validation-thoigiannhap">{validation.thoigiannhap}</div>}
         </div>
         <div className="col-md-6">
           <label htmlFor="soluong" className="form-label">Số lượng loại:<span style={{ color: 'red', marginLeft: '5px' }}>*</span></label>
           <input type="text" className={validation.soluong ? 'form-control is-invalid' : 'form-control'} id="soluong" name="soluong" value={productType.soluong} onChange={(e) => setProductType({...productType, soluong: e.target.value})} required/>
-          {validation.soluong && <div className="invalid-feedback">{validation.soluong}</div>}
+          {validation.soluong && <div className="invalid-feedback validation-soluong">{validation.soluong}</div>}
         </div>
         <div className="col-12">
           <label htmlFor="vitri" className="form-label">Vị trí trưng bày:</label>
